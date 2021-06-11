fed51414f68fe92d040f581929094c2adedd06f4
MarvinGM <marvinmahilum091197@gmail.com>
2021-06-11 12:35:59 +0800

    Initial Commit

---
+++ SwiftUI WebView/SwiftUI_WebViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_WebViewApp.swift
+//  SwiftUI WebView
+//
+//  Created by Amben on 6/11/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_WebViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
